d3f4e5b4b4c2f8d934b6d7587c12836bee8f158c
Anatoly Yakovenko <anatoly@solana.com>
2022-03-10 20:00:32 -0800

    update

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     const num_nodes: usize = 10_000;
     const bad_nodes: usize = 3_333;
     const num_packets: usize = 64;
-    const my_node:usize = 9_999;
+    const my_node: usize = 9_999;
     let mut success: usize = 0;
     let mut total: usize = 0;
 
     for block in 1..10_001 {
-        let mut nodes: [Node; num_nodes] = [Node{shreds : [0; 64]}; 10_000];
+        let mut nodes: [Node; num_nodes] = [Node { shreds: [0; 64] }; 10_000];
         for shred in 0..num_packets {
             let mut rng = ChaCha8Rng::seed_from_u64(shred as u64 * block as u64);
             let mut index: Vec<usize> = (0..num_nodes).into_iter().collect();
@@ -32,7 +32,11 @@ fn main() {
             //lvl 1
             for x in 1..50 {
                 let retransmitter = index[x];
-                for node in &index[x*200..(x + 1)*200] {
+                //skip if node was skipped by a bad node
+                if nodes[retransmitter].shreds[shred] == 0 {
+                    continue;
+                }
+                for node in &index[x * 200..(x + 1) * 200] {
                     if retransmitter < bad_nodes {
                         continue;
                     }
