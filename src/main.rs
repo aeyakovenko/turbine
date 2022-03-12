02240928e2ee19a7fd59f2d04be25e4e9db82b43
Anatoly Yakovenko <anatoly@solana.com>
2022-03-12 09:20:56 -0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
-const BATCH_SIZE: usize = 64;
+const BATCH_SIZE: usize = 96;
+const VOTE_SIZE: usize = 42; // 0.44 * 96
+const RECOVER_SIZE: usize = BATCH_SIZE/3;
 #[derive(Clone, Copy)]
 struct Node {
     shreds: [u8; BATCH_SIZE],
@@ -9,10 +11,12 @@ struct Node {
 
 fn main() {
     const num_nodes: usize = 10_000;
-    const bad_nodes: usize = 500;
+    const bad_nodes: usize = 3_333;
     const num_packets: usize = BATCH_SIZE;
+    const hood_size: usize = 200;
     let mut fails = 0;
     let mut success: usize = 0;
+    let mut blocks = 0;
     let mut total: usize = 0;
 
     for block in 1..10_001 {
@@ -28,18 +32,18 @@ fn main() {
             let retransmitter = index[0];
             // if a bad node, skip retransmitting to lvl 0
             if retransmitter >= bad_nodes {
-                for node in &index[0..200] {
+                for node in &index[0..hood_size] {
                     nodes[*node].shreds[shred] = 1;
                 }
             }
             //lvl 1
-            for x in 1..50 {
+            for x in 1..(num_nodes/hood_size) {
                 let retransmitter = index[x];
                 //skip if node was skipped by a bad node
                 if nodes[retransmitter].shreds[shred] == 0 {
                     continue;
                 }
-                for node in &index[x * 200..(x + 1) * 200] {
+                for node in &index[x * hood_size..(x + 1) * hood_size] {
                     if retransmitter < bad_nodes {
                         continue;
                     }
@@ -47,17 +51,23 @@ fn main() {
                 }
             }
         }
-        let mut all = 0;
+        let mut voted = 0;
         for node in 0..num_nodes {
-            if nodes[node].shreds.into_iter().sum::<u8>() > (BATCH_SIZE / 2) as u8 {
-                all += 1;
+            if nodes[node].shreds.into_iter().sum::<u8>() > VOTE_SIZE  as u8 {
+                voted += 1;
             }
         }
-        if all >= 6_667 || all <= 3_333 {
-            success += 1;
+        let mut recovered = 0;
+        for node in 0..num_nodes {
+            if nodes[node].shreds.into_iter().sum::<u8>() > RECOVER_SIZE as u8 {
+                recovered += 1;
+            }
         }
-        fails += num_nodes - all;
+
+        if voted > 3_3333 && recovered < 6_666 {
+            fails += recovered;
+        }
         total += 1;
-        println!("{} {}/{} {}/{}", all, success, total, fails, total * num_nodes);
+        println!("{} {} {}/{}", voted, recovered, fails, total);
     }
 }
